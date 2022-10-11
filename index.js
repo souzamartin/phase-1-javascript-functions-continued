564c0f12778d549ca8b92b6dcbbbb6e7dcdd8e8a
Martin <souzam@sonic.net>
2022-10-11 18:44:59 -0400

    commit tree 503f1c2087ceb362224a0dc374ffad5e0cd1b9aa

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(decor="*") {
+    return (function (descriptor="special") {
+        return `You are ${decor}${descriptor}${decor}!`
+    })
+}
